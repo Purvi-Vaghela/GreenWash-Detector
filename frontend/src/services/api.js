4af0cb2fc0696cba3fafa233b7b25d3d8a05bf5b
deepsinh231 <solankideepsinh231@gmail.com>
2026-01-11 15:08:14 +0800

    url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://136.113.183.167';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://35.188.209.136';
 
 const api = axios.create({
   baseURL: API_BASE,
